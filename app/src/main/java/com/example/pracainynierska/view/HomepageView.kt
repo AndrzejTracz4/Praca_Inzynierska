fb6427b4cf15824fb9dedb35252f16e4a606160b
andrzejtracz <andrzej.tracz440@wp.pl>
2024-09-24 15:18:11 +0200

    Still: TopBar in HomepageView

--- app/src/main/java/com/example/pracainynierska/view/HomepageView.kt
+++ app/src/main/java/com/example/pracainynierska/view/HomepageView.kt
@@ -3,25 +3,36 @@ package com.example.pracainynierska.view
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.pracainynierska.repository.UserRepository
@@ -35,6 +46,7 @@ fun HomepageView(navController: NavController, userRepository: UserRepository, u
 
     val focusManager = LocalFocusManager.current
 
+
     // Pobranie instancji LoginViewModel przy użyciu LoginViewModelFactory
     val loginViewModel: LoginViewModel = viewModel(
         factory = LoginViewModelFactory(userRepository)
@@ -43,7 +55,7 @@ fun HomepageView(navController: NavController, userRepository: UserRepository, u
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(Color.White) // Tło strony
             .pointerInput(Unit) {
                 detectTapGestures(onTap = { focusManager.clearFocus() })
             }
@@ -51,14 +63,52 @@ fun HomepageView(navController: NavController, userRepository: UserRepository, u
         Scaffold(
             topBar = {
                 TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color(0xFF334166) // Ustawienie koloru tła topbara na czarny
+                    ),
+                    modifier = Modifier
+                        .clip(
+                            RoundedCornerShape(
+                                topStart = 0.dp,
+                                topEnd = 0.dp,
+                                bottomStart = 16.dp,
+                                bottomEnd = 16.dp
+                            )
+                        )
+//                        .border(
+//                            width = 2.dp,
+//                            color = Color(0xFF495D92),
+//                            shape = RoundedCornerShape(
+//                                topStart = 0.dp,
+//                                topEnd = 0.dp,
+//                                bottomStart = 16.dp,
+//                                bottomEnd = 16.dp
+//                            )
+//                        ),
+                            ,
                     title = {
-                        Text(text = username ?: "Loading...") // Wyświetla nazwę użytkownika
+                        Text(
+                            text = username ?: "Loading...",
+                            fontSize = 30.sp,
+                            color = Color.White,
+                            style = TextStyle(
+                                shadow = Shadow(
+                                    color = Color.Black,
+                                    offset = Offset(10f, 12f),
+                                    blurRadius = 3f
+                                )
+                            )
+                        )
                     },
-                    actions = {
+                    navigationIcon = {
                         IconButton(onClick = {
                             // Akcja, np. wylogowanie
                         }) {
-                            Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings")
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu",
+                                tint = Color.White
+                                )
                         }
                     }
                 )
@@ -68,8 +118,8 @@ fun HomepageView(navController: NavController, userRepository: UserRepository, u
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
-            ){
-
+            ) {
+                // Zawartość strony
             }
         }
     }
