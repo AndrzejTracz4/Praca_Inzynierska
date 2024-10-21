7a5d57b9703e9255451d0d45969a7de0daf385f4
andrzejtracz <andrzej.tracz440@wp.pl>
2024-10-21 23:02:45 +0200

    DatePicker in AddTaskView

--- app/src/main/java/com/example/pracainynierska/view/AddTaskView.kt
+++ app/src/main/java/com/example/pracainynierska/view/AddTaskView.kt
@@ -2,6 +2,7 @@ package com.example.pracainynierska.view
 
 import BottomMenu
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -20,6 +21,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -38,9 +40,11 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,9 +65,15 @@ fun AddTaskView(navController: NavController, loginViewModel: LoginViewModel) {
     var taskStartDate by remember { mutableStateOf("") }
     var selectedDifficulty by remember { mutableStateOf("Łatwy") }
     var selectedCategory by remember { mutableStateOf("Samorozwój") }
+    var showDatePicker by remember { mutableStateOf(false) }
+    var showTimePicker by remember { mutableStateOf(false) }
+    var selectedDate by remember { mutableStateOf("") }
+    var selectedTime by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
 
-    Box (
+
+Box (
         modifier = Modifier
             .fillMaxSize()
             .background(
@@ -161,23 +171,82 @@ fun AddTaskView(navController: NavController, loginViewModel: LoginViewModel) {
                     )
                     Spacer(modifier = Modifier.height(4.dp))
 
-                    TextField(
-                        value = taskName,
-                        onValueChange = { taskName = it },
+                    Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .clip(RoundedCornerShape(12.dp)), // Zaokrąglenie boków TextField
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = Color(0x4DFFFFFF),
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent,
-                            cursorColor = Color.White,
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
-                        ),
-                        singleLine = true
+                            .height(55.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                            .background(Color(0x4DFFFFFF)),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        TextField(
+                            value = taskName,
+                            onValueChange = { taskName = it },
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(vertical = 0.dp),
+                            colors = TextFieldDefaults.textFieldColors(
+                                containerColor = Color.Transparent,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                cursorColor = Color.White,
+                                focusedTextColor = Color.White,
+                                unfocusedTextColor = Color.White
+                            ),
+                            singleLine = true,
+                            textStyle = LocalTextStyle.current.copy(
+                                color = Color.White,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.ExtraBold,
+                                textAlign = TextAlign.Center
+                            )
+                        )
+                    }
+
+                }
+
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        Text(
+                            text = "Data rozpoczęcia",
+                            color = Color.White,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        CustomDatePickerField(
+                            text = "",
+                            value = selectedDate,
+                            onValueChange = { selectedDate = it },
+                            onClick = { showDatePicker = true }
+                        )
+                    }
+                }
+
+
+
+
+
+
+
+
+
+                // DatePickerDialog and TimePickerDialog implementation
+                if (showDatePicker) {
+                    DatePickerDialog(
+                        onDateSelected = { date ->
+                            selectedDate = date
+                            showDatePicker = false
+                        },
+                        onDismissRequest = { showDatePicker = false }
                     )
                 }
+
 
             }
         }
@@ -218,5 +287,35 @@ fun CustomAddTaskButton(text: String, isSelected: Boolean, onClick: () -> Unit, 
                 fontWeight = FontWeight.ExtraBold
             )
         }
+    }
+}
+
+@Composable
+fun CustomDatePickerField(
+    text: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    onClick: () -> Unit,
+    height: Dp = 50.dp
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(height)
+            .padding(vertical = 4.dp)
+            .background(
+                color = Color(0x4DFFFFFF),
+                shape = RoundedCornerShape(12.dp)
+            )
+            .clickable { onClick() }
+            .padding(horizontal = 16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = value.ifEmpty { text },
+            color = Color.White,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
     }
 }
