06dd5b097c304bae16e24695c4965d8d7b6650e6
andrzejtracz <andrzej.tracz440@wp.pl>
2025-01-05 00:21:32 +0100

    Empty box if there is no ActiveBoosters

--- app/src/main/java/com/example/pracainynierska/ui_view_components/components/BoosterList.kt
+++ app/src/main/java/com/example/pracainynierska/ui_view_components/components/BoosterList.kt
@@ -1,11 +1,23 @@
 package com.example.pracainynierska.ui_view_components.components
 
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.pracainynierska.view_model.BoosterViewModel
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun BoosterList(boosterViewModel: BoosterViewModel) {
     val boostersList = boosterViewModel.boosters.observeAsState(initial = emptyList()).value
@@ -28,5 +40,15 @@ fun BoosterList(boosterViewModel: BoosterViewModel) {
                 }
             }
         )
-    }
+    }else
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(
+                    color = Color(0x14FFFFFF),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(4.dp)
+        )
 }
